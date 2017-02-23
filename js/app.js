38a7880452b76687f83132bbe49f590d271d4857
Marie-Maxime Tanguay <mariemaximetanguay@gmail.com>
2017-02-22 19:09:45 -0500

    animation des etoiles avec JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,31 @@
-var APP = {} || APP;
+var arbres
+var taille = 1;
 
-APP.paralax = {
+function getTranformTranslate(element){
+    string = element.getAttribute("transform");
+    return string.substring(string.indexOf("(")+1, string.indexOf(")")).split(" ");
+}
 
+function animRandomEtoile(){
+    
+}
+
+function scaleEtoile() {
+    var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+    var trace = taille * fractionDuScroll +1
+    for (var i = 0; i < arbres.length; i++) {
+            var translate = getTranformTranslate(arbres[i]);
+            arbres[i].style.transform = "translateX("+ translate[0] +"px) translateY("+ translate[1] +"px) scale("+trace+")";
+    }
+}
+
+function changerTailleDesArbres(){
+    // on cible l'élément « path » que l'on veut animer
+    arbres = document.getElementsByClassName("uneEtoile");
+    window.addEventListener("scroll", scaleEtoile);
 }
 
 window.onload = function() {
     var rellax = new Rellax('.rellax');
+    changerTailleDesArbres();
 };
